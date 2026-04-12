da668140eff971a51b70d9cb9ecd56b4f0bc0d87
Vasilii Nikonov <62755968+vnikonov63@users.noreply.github.com>
2026-04-11 21:38:24 -0700

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int p1 = 0, p2 = numbers.size() - 1;
+        while (p1 < p2) {
+            if (numbers[p1] + numbers[p2] == target) {
+                return {p1 + 1, p2 + 1};
+            }
+            else if (numbers[p1] + numbers[p2] > target) {
+                p2--;
+            }
+            else {
+                p1++;
+            }
+        }
+
+        return {0, 0}; /* There is an answer, it does not matter */
+    }
+};
