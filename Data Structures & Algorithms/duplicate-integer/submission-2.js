5b137511be2efb785196b231351c9cea537e0294
Vasilii Nikonov <62755968+vnikonov63@users.noreply.github.com>
2026-04-17 15:48:18 -0700

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        nums.sort((a, b) => a - b);
+
+        for (let i = 1; i < nums.length; ++i) {
+            if (nums[i] == nums[i - 1]) return true;
+        }
+
+        return false;
+    }
+}
